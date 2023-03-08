988edad0b0a53c9f316282f9e4c07b851949e9ef
Abhishek Sharma <abhisheksharma@Abhisheks-MacBook-Air.local>
2023-03-08 19:05:00 +0530

    dfdsfad

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,10 +6,14 @@ from django.test import SimpleTestCase
 
 from app import calc
 
-"""defining class for test"""
+
+
 class CalculatorTest(SimpleTestCase):
    """ check number additon"""
-def test_number_addition(self):
-    res = calc.addNumbers(4, 7)
-    self.assertEqual(res, 10)
+
+   def test_number_addition(self):
+      """jjhjh"""
+      res = calc.addNumbers(4, 7)
+
+      self.assertEqual(res, 11)
 
